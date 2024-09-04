3c5308b4c3f86c94bcef4d67615f797af8af13c0
DEV-COM <kwp@tlen.pl>
2024-09-04 13:18:48 +0200

    2

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,6 +1,13 @@
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
+import java.time.Duration;
+
+import static java.time.Duration.ofMillis;
+import static java.time.Duration.ofMinutes;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 //@Disabled("All test in the class wiil be skipped")
 public class MyFirstTest {
@@ -15,7 +22,9 @@ public class MyFirstTest {
         System.out.println("Setup EACH TEST in the class");
     }
 
+    @DisabledOnOs({OS.MAC,OS.LINUX})
     @Test
+    @Tag("simple")
     @DisplayName("Custom test name containing spaces")
     void mySimpleTest() {
         System.out.println("TEST 1");
@@ -29,24 +38,61 @@ public class MyFirstTest {
     }
 
     @Test
+    void assumeTrueTest(){
+        assumeTrue(true);
+    }
+
+    @Test
+    @Tag("display")
+    @Tag("performance")
     @DisplayName("\uD83D\uDC80")
     void testWithDisplayNameContainingEmoji() {
-        assertEquals(2,2);
-        assertTrue(true,"wiadomość");
-        assertFalse(false,"wiadomość");
+        assertEquals(2, 2);
+        assertTrue(true, "wiadomość");
+        assertFalse(false, "wiadomość");
     }
 
     @Test
-    void groupedAssertions(){
-        assertAll("address", () -> assertEquals("John","John"),
-                () -> assertTrue(true,"wiadomość"));
+    void groupedAssertions() {
+        assertAll("address", () -> assertEquals("John", "John"),
+                () -> assertTrue(true, "wiadomość"));
     }
 
     @Test
-    void groupedAssertions_tdd(){
+    void groupedAssertions_tdd() {
         Address address = new Address("Jan", "Kowalski");
-        assertAll("address", () -> assertEquals("Jan",address.getFirstName()),
+        assertAll("address", () -> assertEquals("Jan", address.getFirstName()),
                 () -> assertEquals("Kowalski", address.getLastName()));
+    }
+
+    @Test
+    void exceptionTesting() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            throw new IllegalArgumentException("wiadomość");
+        });
+        assertEquals("wiadomość", exception.getMessage());
+    }
+
+    @Test
+    void timeoutNotExceeded() {
+        assertTimeout(ofMinutes(2), () -> {
+//            task
+        });
+    }
+
+    @Test
+    void timeoutExceeded() {
+        assertTimeout(ofMillis(10), () -> {
+            Thread.sleep(100);
+        });
+    }
+
+    @Test
+    void timeoutNotExceededWithResult(){
+        String actualResult = assertTimeout(ofMinutes(1), () -> {
+            return "cześć!";
+        });
+        assertEquals("cześć!", actualResult);
     }
 
 
@@ -59,4 +105,5 @@ public class MyFirstTest {
     static void teardownAll() {
         System.out.println("Teardown ALL TEST in the class");
     }
+
 }
